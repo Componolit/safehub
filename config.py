919a55fa15b74f833a9be1b877c6bc099172a42a
Johannes Kliemann <jk@jkliemann.de>
2017-06-26 15:15:22 +0200

    Added determination of correct base directory.

---
+++ config.py
@@ -0,0 +1,35 @@
+
+import os
+import re
+
+def get_base_path():
+    with open('/etc/login.defs', 'r') as login_defs:
+        return _get_base_path(login_defs.readlines(),
+                              lambda: os.getenv("HOME"),
+                              lambda: os.getuid())
+
+def _get_base_path(login_defs, home, uid):
+    UID_MIN = 1000
+    UID_MAX = 60000
+    SYS_UID_MIN = 100
+    SYS_UID_MAX = 999
+    if uid == 0:
+        return "/" + home.strip("/") + "/.safehub/"
+    tabsplit = re.compile("\t+| +")
+    for line in login_defs:
+        line = line.strip('\n')
+        if line.startswith("UID_MIN"):
+            UID_MIN = int(tabsplit.split(line)[1])
+        elif line.startswith("UID_MAX"):
+            UID_MAX = int(tabsplit.split(line)[1])
+        elif line.startswith("SYS_UID_MIN"):
+            SYS_UID_MIN = int(tabsplit.split(line)[1])
+        elif line.startswith("SYS_UID_MAX"):
+            SYS_UID_MAX = int(tabsplit.split(line)[1])
+    if uid <= UID_MAX and uid >= UID_MIN:
+        return "/" + home.strip("/") + "/.safehub/"
+    elif uid <= SYS_UID_MAX and uid >= SYS_UID_MIN:
+        return "/" + home.strip("/") + "/"
+    else:
+        raise ValueError("UID outside defined UID ranges.")
+
--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import os
 import glob
 import py_compile
 
+from config import _get_base_path
+
 class Compile(unittest.TestCase):
 
     files = []
@@ -12,8 +14,18 @@ class Compile(unittest.TestCase):
     def setUp(self):
         cwd = os.path.dirname(os.path.realpath(__file__))
         self.files = glob.glob("{}/**/*.py".format(cwd), recursive=True)
-        print(self.files)
 
     def test_compile(self):
         for f in self.files:
             py_compile.compile(f, doraise=True)
+
+class Config(unittest.TestCase):
+
+    def test_base(self):
+        assert("/home/test/.safehub/" == _get_base_path([], "/home/test", 1000))
+        assert("/var/lib/safehub/" == _get_base_path([], "/var/lib/safehub", 499))
+        assert("/var/lib/safehub/" == _get_base_path(["UID_MIN     2000\n", "SYS_UID_MAX\t\t\t1999\n"], "/var/lib/safehub", 1500))
+        assert("/home/test/.safehub/" == _get_base_path(["SYS_UID_MAX        400\n", "UID_MIN\t\t450\n"], "/home/test", 450))
+        assert("/var/lib/safehub/" == _get_base_path(["UID_MIN\t\t\t2000\n", "SYS_UID_MAX     1999\n"], "/var/lib/safehub", 1500))
+        assert("/home/test/.safehub/" == _get_base_path(["SYS_UID_MAX\t\t400\n", "UID_MIN    450\n"], "/home/test", 450))
+        assert("/root/.safehub/" == _get_base_path([], "/root", 0))
