22f67cb518ecd4d0ee070985a49420942e4b957f
Johannes Kliemann <jk@jkliemann.de>
2017-06-27 16:56:27 +0200

    Refactored tests.

--- config.py
+++ config.py
@@ -2,49 +2,78 @@
 import os
 import re
 
+
+
 def get_base_path():
     with open('/etc/login.defs', 'r') as login_defs:
         return _get_base_path(login_defs.readlines(),
                               lambda: os.getenv("HOME"),
                               lambda: os.getuid())
 
-def _get_base_path(login_defs, home, uid):
-    UID_MIN = 1000
-    UID_MAX = 60000
-    SYS_UID_MIN = 100
-    SYS_UID_MAX = 999
-    if uid == 0:
-        return "/" + home.strip("/") + "/.safehub"
-    tabsplit = re.compile("\t+| +")
-    for line in login_defs:
-        line = line.strip('\n')
-        if line.startswith("UID_MIN"):
-            UID_MIN = int(tabsplit.split(line)[1])
-        elif line.startswith("UID_MAX"):
-            UID_MAX = int(tabsplit.split(line)[1])
-        elif line.startswith("SYS_UID_MIN"):
-            SYS_UID_MIN = int(tabsplit.split(line)[1])
-        elif line.startswith("SYS_UID_MAX"):
-            SYS_UID_MAX = int(tabsplit.split(line)[1])
-    if uid <= UID_MAX and uid >= UID_MIN:
-        return "/" + home.strip("/") + "/.safehub"
-    elif uid <= SYS_UID_MAX and uid >= SYS_UID_MIN:
-        return "/" + home.strip("/")
-    else:
-        raise ValueError("UID outside defined UID ranges.")
+class ConfigBase:
 
 
-def load_config():
-    cfg = None
-    try:
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        cfg = open(dir_path + "/safehub.json", "r")
-    except FileNotFoundError:
+    def get_login_defs(self):
+        raise NotImplementedError("get_login_defs")
+
+    def get_home(self):
+        raise NotImplementedError("get_home")
+
+    def get_uid(self):
+        raise NotImplementedError("get_uid")
+
+    def get_base_path(self):
+        login_defs = self.get_login_defs()
+        home = self.get_home()
+        uid = self.get_uid()
+        UID_MIN = 1000
+        UID_MAX = 60000
+        SYS_UID_MIN = 100
+        SYS_UID_MAX = 999
+        if uid == 0:
+            return "/" + home.strip("/") + "/.safehub"
+        tabsplit = re.compile("\t+| +")
+        for line in login_defs:
+            line = line.strip('\n')
+            if line.startswith("UID_MIN"):
+                UID_MIN = int(tabsplit.split(line)[1])
+            elif line.startswith("UID_MAX"):
+                UID_MAX = int(tabsplit.split(line)[1])
+            elif line.startswith("SYS_UID_MIN"):
+                SYS_UID_MIN = int(tabsplit.split(line)[1])
+            elif line.startswith("SYS_UID_MAX"):
+                SYS_UID_MAX = int(tabsplit.split(line)[1])
+        if uid <= UID_MAX and uid >= UID_MIN:
+            return "/" + home.strip("/") + "/.safehub"
+        elif uid <= SYS_UID_MAX and uid >= SYS_UID_MIN:
+            return "/" + home.strip("/")
+        else:
+            raise ValueError("UID outside defined UID ranges.")
+
+
+    def load_config(self):
+        cfg = None
         try:
-            cfg = open(os.getenv("HOME") + "/.safehub.json", "r")
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            cfg = open(dir_path + "/safehub.json", "r")
         except FileNotFoundError:
             try:
-                cfg = open("/etc/safehub/safehub.json", "r")
+                cfg = open(os.getenv("HOME") + "/.safehub.json", "r")
             except FileNotFoundError:
-                raise FileNotFoundError("Failed to find configuration file.")
-    return json.load(cfg)
+                try:
+                    cfg = open("/etc/safehub/safehub.json", "r")
+                except FileNotFoundError:
+                    raise FileNotFoundError("Failed to find configuration file.")
+        return json.load(cfg)
+
+class Config(ConfigBase):
+
+    def get_login_defs(self):
+        with open('/etc/login.defs', 'r') as login_defs:
+            return login_defs.readlines()
+
+    def get_home(self):
+        return os.getenv("HOME")
+
+    def get_uid(self):
+        return os.getuid()
--- test.py
+++ test.py
@@ -4,8 +4,10 @@ import unittest
 import os
 import glob
 import py_compile
+from subprocess import Popen, PIPE
 
-from config import _get_base_path
+from config import ConfigBase
+from git import Git
 from repository import _parse_url, _gen_repo_git_url, _gen_wiki_git_url, _gen_path
 
 class Compile(unittest.TestCase):
@@ -20,17 +22,108 @@ class Compile(unittest.TestCase):
         for f in self.files:
             py_compile.compile(f, doraise=True)
 
-class Config(unittest.TestCase):
+class ConfigTest(unittest.TestCase, ConfigBase):
+
+    def get_login_defs(self):
+        return self.login_defs
+
+    def get_home(self):
+        return self.home
+
+    def get_uid(self):
+        return self.uid
 
     def test_base(self):
-        self.assertEqual("/home/test/.safehub", _get_base_path([], "/home/test", 1000))
-        self.assertEqual("/var/lib/safehub", _get_base_path([], "/var/lib/safehub", 499))
-        self.assertEqual("/var/lib/safehub", _get_base_path(["UID_MIN     2000\n", "SYS_UID_MAX\t\t\t1999\n"], "/var/lib/safehub", 1500))
-        self.assertEqual("/home/test/.safehub", _get_base_path(["SYS_UID_MAX        400\n", "UID_MIN\t\t450\n"], "/home/test", 450))
-        self.assertEqual("/var/lib/safehub", _get_base_path(["UID_MIN\t\t\t2000\n", "SYS_UID_MAX     1999\n"], "/var/lib/safehub", 1500))
-        self.assertEqual("/home/test/.safehub", _get_base_path(["SYS_UID_MAX\t\t400\n", "UID_MIN    450\n"], "/home/test", 450))
-        self.assertEqual("/root/.safehub", _get_base_path([], "/root", 0))
-        self.assertRaises(ValueError, _get_base_path, ["UID_MIN   2000\n"], "/", 1000)
+
+        self.login_defs = []  
+        self.home = "/home/test"  
+        self.uid = 1000 
+        self.assertEqual("/home/test/.safehub", self.get_base_path())
+            
+        self.login_defs = []  
+        self.home = "/var/lib/safehub"  
+        self.uid = 499 
+        self.assertEqual("/var/lib/safehub", self.get_base_path())
+        
+        self.login_defs = ["UID_MIN     2000\n", "SYS_UID_MAX\t\t\t1999\n"]  
+        self.home = "/var/lib/safehub"  
+        self.uid = 1500 
+        self.assertEqual("/var/lib/safehub", self.get_base_path())
+        
+        self.login_defs = ["SYS_UID_MAX        400\n", "UID_MIN\t\t450\n"]  
+        self.home = "/home/test"  
+        self.uid = 450 
+        self.assertEqual("/home/test/.safehub", self.get_base_path())
+        
+        self.login_defs = ["UID_MIN\t\t\t2000\n", "SYS_UID_MAX     1999\n"]  
+        self.home = "/var/lib/safehub"  
+        self.uid = 1500 
+        self.assertEqual("/var/lib/safehub", self.get_base_path())
+        
+        self.login_defs = ["SYS_UID_MAX\t\t400\n", "UID_MIN    450\n"]  
+        self.home = "/home/test"  
+        self.uid = 450 
+        self.assertEqual("/home/test/.safehub", self.get_base_path())
+        
+        self.login_defs = []  
+        self.home = "/root"  
+        self.uid = 0 
+        self.assertEqual("/root/.safehub", self.get_base_path())
+       
+        self.login_defs = ["UID_MIN   2000\n"]
+        self.home = "/"
+        self.uid = 1000
+        self.assertRaises(ValueError, self.get_base_path)
+    
+    def test_impl(self):
+        baseconfig = ConfigBase()
+        self.assertRaises(NotImplementedError, baseconfig.get_base_path)
+
+class GitTest(unittest.TestCase):
+
+    def setUp(self):
+        self.basepath = '/tmp/safehub_git_test.{}/'.format(os.getpid())
+        if not os.path.isdir(self.basepath):
+            os.mkdir(self.basepath)
+        self.repo = self.basepath + 'repo'
+        self.bare = self.basepath + 'bare'
+        if not os.path.isdir(self.repo):
+            os.mkdir(self.repo)
+        if not os.path.isdir(self.bare):
+            os.mkdir(self.bare)
+        Popen(['git', 'init'], cwd=self.repo, stdout=PIPE).communicate()
+        Popen(['git', 'init'], cwd=self.bare, stdout=PIPE).communicate()
+
+    def test_init(self):
+        Git.init(self.basepath + 'inittest')
+        p = Popen(['git', 'branch'], cwd=self.basepath+'inittest', stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        if not p.returncode == 0:
+            raise RuntimeError("Failed to init repository: {}".format(err))
+
+    def test_clone(self):
+        Git.clone(self.bare, self.basepath + 'clonetest')
+        p = Popen(['git', 'status'], cwd=self.basepath+'clonetest', stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        if not p.returncode == 0:
+            raise RuntimeError("Failed to clone repository: {}".format(err))
+
+
+    def test_add(self):
+        with open(self.repo + '/testfile', 'w') as testfile:
+            testfile.write('test\n')
+        repo = Git(self.repo)
+        repo.add()
+        p = Popen(['git', 'status', '-s'], cwd=self.repo, stdout=PIPE, stderr=PIPE)
+        out, err =  p.communicate()
+        out = out.decode('utf-8')
+        repo.commit("testcommit")
+        for entry in out.split('\n'):
+            if entry.startswith('??'):
+                raise ValueError("No untracked files should exist: {}".format(err))
+
+
+
 
 class Github(unittest.TestCase):
 
